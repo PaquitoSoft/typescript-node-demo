665945b39c0bb99ad0d786e41ddd52a7e179425d
PaquitoSoft <paquitosoftware@gmail.com>
2021-02-20 14:22:13 +0100

    remove dist folder

--- dist/models/todo.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Todo {
-    constructor(id, title) {
-        this.id = id;
-        this.title = title;
-    }
-}
-exports.default = Todo;
